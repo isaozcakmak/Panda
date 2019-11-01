3e8e186b60b3e55963bb7ab983de67654b64646c
Isa Ozcakmak <isaozcakmak79@gmail.com>
2019-11-01 21:53:27 +0300

    Logger class is updated

--- src/Utility/Logger.h
+++ src/Utility/Logger.h
@@ -1,11 +1,19 @@
 #ifndef PANDA_UTILITY_LOGGER
 #define PANDA_UTILITY_LOGGER
 
-namespace Koala::Utility
+#include <Utility/File.h>
+
+namespace Utility
 {
 	class Logger
 	{
 	public:
+		static void Initialize(const std::string& projectName)
+		{
+			Instance();
+			logFilePath();
+		}
+
 		static Logger& Instance()
 		{
 			static Logger instance;
@@ -14,6 +22,16 @@ namespace Koala::Utility
 
 	private:
 		Logger();
+
+		static const std::string& logFilePath()
+		{
+			static const std::string path = "Data/user.log";
+			return path;
+		}
+
+	private:
+		File m_file = File(logFilePath(), true);
+
 	};
 }
 #endif // PANDA_UTILITY_LOGGER
