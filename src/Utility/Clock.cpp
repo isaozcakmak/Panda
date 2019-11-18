398df7937f2244fb8f114939b101669698ed608c
Isa Ozcakmak <isaozcakmak79@gmail.com>
2019-11-18 20:28:22 +0300

    Clock class is updated

--- src/Utility/Clock.cpp
+++ src/Utility/Clock.cpp
@@ -1,9 +1,24 @@
 #include "Clock.h"
 
+#include <chrono>
+#include <ctime>
+
 namespace Utility
 {
 	Clock::Clock(int hour, int minute, int second) : 
 		m_hour(hour), m_minute(minute), m_second(second)
 	{
+	}
+	const std::string Clock::Now()
+	{
+		auto now = std::chrono::system_clock::now();
+		auto now_c = std::chrono::system_clock::to_time_t(now);
+		auto tmDate = std::localtime(&now_c);
+
+		return std::string();
+	}
+	const std::string Clock::ToString() const
+	{
+		return std::string();
 	}
 }
