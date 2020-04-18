db3bba676be896d507194ecdeb25a9d1495470a5
Isa Ozcakmak <isaozcakmak79@gmail.com>
2020-04-18 17:19:13 +0300

    Comments deleted

--- src/Utility/Interpreter.cpp
+++ src/Utility/Interpreter.cpp
@@ -10,16 +10,6 @@ namespace Utility
 
 	void Interpreter::run()
 	{
-		//Utility::Command command = getCommand();
-
-		//switch (command)
-		//{
-		//case Utility::Command::print:
-		//	std::cout << getPrintString() << std::endl;
-		//	break;
-		//default:
-		//	break;
-		//}
 
 		if (m_code.size() == 0)
 			return;
@@ -60,30 +50,6 @@ namespace Utility
 		}
 
 		std::cout << result << std::endl;
-
-		//int rightValue = getNumber(m_code[0], index);
-		//Utility::Command operatorValue = getOperator(m_code[0], index);
-		//int leftValue = getNumber(m_code[0], index);
-
-		//int rightValue = getDigit(m_code[0].at(0));
-		//Utility::Command operatorValue = getOperator(m_code[0].at(1));
-		//int leftValue = getDigit(m_code[0].at(2));
-		
-		//switch (operatorValue)
-		//{
-		//	case Utility::Command::plus:
-		//		std::cout << rightValue + leftValue << std::endl;
-		//		break;
-		//	case Utility::Command::minus:
-		//		std::cout << rightValue - leftValue << std::endl;
-		//		break;
-		//	case Utility::Command::print:
-		//		break;
-		//	case Utility::Command::unknown:
-		//		break;
-		//	default:
-		//		break;
-		//}
 
 	}
 
@@ -178,33 +144,5 @@ namespace Utility
 		}
 
 	}
-
-	//Utility::Command Interpreter::getCommand()
-	//{
-	//	std::string commandStr = m_code.substr(0, m_code.find_first_of(' '));
-	//	Utility::Command command = Utility::Command::unknown;
-
-	//	if (commandStr == "print")
-	//	{
-	//		command = Utility::Command::print;
-	//	}
-
-	//	return command;
-	//}
-
-	//std::string Interpreter::getPrintString()
-	//{
-	//	std::string str = "";
-
-	//	for (size_t i = m_code.find_first_of('"'); i < m_code.find_last_of('"'); i++)
-	//	{
-	//		if ( m_code[i] != '"' )
-	//		{
-	//			str += m_code[i];
-	//		}
-	//	}
-	//	
-	//	return str;
-	//}
 
 }
