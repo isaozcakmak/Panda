92c28bfdbbd0fd635ecb82f4836e2151b167a5aa
Isa Ozcakmak <isaozcakmak79@gmail.com>
2020-05-16 11:46:27 +0300

    AbstractSyntaxTreeAssignNode class added

---
+++ src/AbstractSyntaxTreeAssignNode.cpp
@@ -0,0 +1,32 @@
+#include <AbstractSyntaxTreeAssignNode.h>
+#include <iostream>
+
+AbstractSyntaxTreeAssignNode::AbstractSyntaxTreeAssignNode(AbstractSyntaxTree* left, Token op, AbstractSyntaxTree* right) :
+	m_nodeType(NodeType::BinOp),
+	m_left(left),
+	m_op(op),
+	m_right(right)
+{
+}
+
+AbstractSyntaxTree::NodeType AbstractSyntaxTreeAssignNode::getNodeType()
+{
+	return m_nodeType;
+}
+
+Token::TokenType AbstractSyntaxTreeAssignNode::getOpTokenType()
+{
+	return m_op.getType();
+}
+
+AbstractSyntaxTree* AbstractSyntaxTreeAssignNode::getLeft()
+{
+	return m_left;
+}
+
+AbstractSyntaxTree* AbstractSyntaxTreeAssignNode::getRight()
+{
+	return m_right;
+}
+
+
---
+++ src/AbstractSyntaxTreeAssignNode.h
@@ -0,0 +1,29 @@
+#ifndef PANDA_ABSTRACTSYNTAXTREEASSIGNNODE
+#define PANDA_ABSTRACTSYNTAXTREEASSIGNNODE
+
+#include <string>
+#include <vector>
+#include <Token.h>
+#include <AbstractSyntaxTree.h>
+
+class AbstractSyntaxTreeAssignNode : public AbstractSyntaxTree
+{
+public:
+	AbstractSyntaxTreeAssignNode(AbstractSyntaxTree* left, Token op, AbstractSyntaxTree* right);
+	~AbstractSyntaxTreeAssignNode() {}
+
+	virtual NodeType getNodeType();
+	Token::TokenType getOpTokenType();
+	AbstractSyntaxTree* getLeft();
+	AbstractSyntaxTree* getRight();
+
+private:
+	NodeType m_nodeType;
+
+	AbstractSyntaxTree* m_left;
+	AbstractSyntaxTree* m_right;
+	Token m_op;
+};
+
+
+#endif // PANDA_ABSTRACTSYNTAXTREEASSIGNNODE
