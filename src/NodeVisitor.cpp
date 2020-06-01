50e3c605c1f4bf0fdb73303d5ddb5c6f93a817bf
Isa Ozcakmak <isaozcakmak79@gmail.com>
2020-06-01 17:21:57 +0300

    NodeVisitor updated

--- src/NodeVisitor.cpp
+++ src/NodeVisitor.cpp
@@ -20,11 +20,37 @@ int NodeVisitor::visit(AbstractSyntaxTree* node)
 	case AbstractSyntaxTree::NodeType::UnaryOp:
 		return visitUnaryOp(node);
 		break;
+	case AbstractSyntaxTree::NodeType::Compound:
+		visitCompound(node);
+		break;
+	case AbstractSyntaxTree::NodeType::Assign:
+		visitAssign(node);
+		break;
+	case AbstractSyntaxTree::NodeType::Var:
+		return visitVar(node);
+		break;
+	case AbstractSyntaxTree::NodeType::NoOp:
+		visitNoOp(node);
+		break;
 	default:
 		break;
 	}
 
 	return 0;
+}
+
+std::unordered_map<std::string, int> NodeVisitor::getVariables()
+{
+	return m_variables;
+}
+
+void NodeVisitor::printVariables()
+{
+	for (auto variable : m_variables)
+	{
+		std::cout << "Variable Name: " << variable.first <<
+			" Variable Value: " << variable.second << std::endl;
+	}
 }
 
 int NodeVisitor::visitNum(AbstractSyntaxTree* node)
@@ -100,5 +126,80 @@ int NodeVisitor::visitUnaryOp(AbstractSyntaxTree* node)
 
 	throw std::exception("AbstractSyntaxTree* Is Not AbstractSyntaxTreeUnaryOpNode*");
 	return 0;
+}
+
+void NodeVisitor::visitCompound(AbstractSyntaxTree* node)
+{
+	auto nodeType = node->getNodeType();
+	if (nodeType == AbstractSyntaxTree::NodeType::Compound)
+	{
+		auto compoundNode = reinterpret_cast<AbstractSyntaxTreeCompoundNode*>(node);
+
+		auto children = compoundNode->getChildren();
+
+		for (size_t i = 0; i < children.size(); i++)
+		{
+			visit(children[i]);
+		}
+
+		return;
+	}
+
+	throw std::exception("AbstractSyntaxTree* Is Not AbstractSyntaxTreeCompoundNode*");
+	return;
+}
+
+void NodeVisitor::visitAssign(AbstractSyntaxTree* node)
+{
+	auto nodeType = node->getNodeType();
+	if (nodeType == AbstractSyntaxTree::NodeType::Assign)
+	{
+		auto assignNode = reinterpret_cast<AbstractSyntaxTreeAssignNode*>(node);
+		auto leftNode = reinterpret_cast<AbstractSyntaxTreeVarNode*>(assignNode->getLeft());
+
+		auto variableName = leftNode->getTokenString();
+
+		m_variables[variableName] = visit(assignNode->getRight());
+		return;
+	}
+
+	throw std::exception("AbstractSyntaxTree* Is Not AbstractSyntaxTreeAssignNode*");
+	return;
+}
+
+int NodeVisitor::visitVar(AbstractSyntaxTree* node)
+{
+	auto nodeType = node->getNodeType();
+	if (nodeType == AbstractSyntaxTree::NodeType::Var)
+	{
+		auto varNode = reinterpret_cast<AbstractSyntaxTreeVarNode*>(node);
+		auto variableName = varNode->getTokenString();
+
+		if (m_variables.contains(variableName))
+		{
+			return m_variables[variableName];
+		}
+		else
+		{
+			std::string errorMessage = variableName + " Variable Does Not Exist";
+			throw std::exception(errorMessage.c_str());
+			return 0;
+		}
+	}
+
+	throw std::exception("AbstractSyntaxTree* Is Not AbstractSyntaxTreeVarNode*");
+	return 0;
+}
+
+void NodeVisitor::visitNoOp(AbstractSyntaxTree* node)
+{
+	auto nodeType = node->getNodeType();
+	if (nodeType == AbstractSyntaxTree::NodeType::NoOp)
+	{
+		return;
+	}
+
+	throw std::exception("AbstractSyntaxTree* Is Not AbstractSyntaxTreeNoOpNode*");
+	return;
 }
 
--- src/NodeVisitor.h
+++ src/NodeVisitor.h
@@ -3,10 +3,16 @@
 
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <iostream>
 #include <AbstractSyntaxTree.h>
 #include <AbstractSyntaxTreeNodes/AbstractSyntaxTreeNumNode.h>
 #include <AbstractSyntaxTreeNodes/AbstractSyntaxTreeBinOpNode.h>
 #include <AbstractSyntaxTreeNodes/AbstractSyntaxTreeUnaryOpNode.h>
+#include <AbstractSyntaxTreeNodes/AbstractSyntaxTreeAssignNode.h>
+#include <AbstractSyntaxTreeNodes/AbstractSyntaxTreeCompoundNode.h>
+#include <AbstractSyntaxTreeNodes/AbstractSyntaxTreeVarNode.h>
+#include <AbstractSyntaxTreeNodes/AbstractSyntaxTreeNoOpNode.h>
 
 class NodeVisitor
 {
@@ -15,12 +21,20 @@ public:
 	~NodeVisitor() {}
 
 	int visit(AbstractSyntaxTree* node);
+	std::unordered_map<std::string, int> getVariables();
+	void printVariables();
 
 private:
 	int visitNum(AbstractSyntaxTree* node);
 	int visitBinOp(AbstractSyntaxTree* node);
 	int visitUnaryOp(AbstractSyntaxTree* node);
+	void visitCompound(AbstractSyntaxTree* node);
+	void visitAssign(AbstractSyntaxTree* node);
+	int visitVar(AbstractSyntaxTree* node);
+	void visitNoOp(AbstractSyntaxTree* node);
 
+private:
+	std::unordered_map<std::string, int> m_variables;
 };
 
 
