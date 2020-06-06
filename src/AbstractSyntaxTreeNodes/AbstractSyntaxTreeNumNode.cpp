db2d0701be633d453e7bdcfca692b3aef1db2f7c
Isa Ozcakmak <isaozcakmak79@gmail.com>
2020-06-06 12:25:42 +0300

    AbstractSyntaxTreeNumOpNode moved

---
+++ src/AbstractSyntaxTreeNodes/AbstractSyntaxTreeNumNode.cpp
@@ -0,0 +1,19 @@
+#include <AbstractSyntaxTreeNodes/AbstractSyntaxTreeNumNode.h>
+#include <iostream>
+
+AbstractSyntaxTreeNumNode::AbstractSyntaxTreeNumNode(Token token) :
+	m_nodeType(NodeType::Num),
+	m_token(token)
+{
+}
+
+AbstractSyntaxTree::NodeType AbstractSyntaxTreeNumNode::getNodeType()
+{
+	return m_nodeType;
+}
+
+int AbstractSyntaxTreeNumNode::getTokenValue()
+{
+	return m_token.getValue();
+}
+
