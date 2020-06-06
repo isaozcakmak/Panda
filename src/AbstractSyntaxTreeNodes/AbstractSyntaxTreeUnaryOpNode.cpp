2db8a615458fd7865d3750964031e3bfd8da0d4c
Isa Ozcakmak <isaozcakmak79@gmail.com>
2020-06-06 12:26:23 +0300

    AbstractSyntaxTreeUnaryOpNode moved

---
+++ src/AbstractSyntaxTreeNodes/AbstractSyntaxTreeUnaryOpNode.cpp
@@ -0,0 +1,25 @@
+#include <AbstractSyntaxTreeNodes/AbstractSyntaxTreeUnaryOpNode.h>
+#include <iostream>
+
+AbstractSyntaxTreeUnaryOpNode::AbstractSyntaxTreeUnaryOpNode(Token op, AbstractSyntaxTree* expr) :
+	m_nodeType(NodeType::UnaryOp),
+	m_op(op),
+	m_expr(expr)
+{
+}
+
+AbstractSyntaxTree::NodeType AbstractSyntaxTreeUnaryOpNode::getNodeType()
+{
+	return m_nodeType;
+}
+
+Token::TokenType AbstractSyntaxTreeUnaryOpNode::getOpTokenType()
+{
+	return m_op.getType();
+}
+
+AbstractSyntaxTree* AbstractSyntaxTreeUnaryOpNode::getExpr()
+{
+	return m_expr;
+}
+
