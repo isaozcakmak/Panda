9b30c21b805d2cacd80cb0500bca21925682dd7b
Isa Ozcakmak <isaozcakmak79@gmail.com>
2020-06-20 11:30:50 +0300

    Logger add method added

--- src/Utility/Logger.cpp
+++ src/Utility/Logger.cpp
@@ -7,11 +7,19 @@
 
 namespace Utility
 {
+	void Logger::add(const std::string& function, size_t line, const std::string& message)
+	{
+		std::string logMessage = message +
+			" {" + function + "::" + std::to_string(line) + "}" +
+			" [" + Clock::ProgramClock().toString() + "]";
+
+		m_file.writeLine(logMessage);
+	}
 
 	Logger::Logger()
 	{
 		std::string logMessage = "\r\n-------------------------------------------------------\r\n\r\n";
-		logMessage += "Panda - " + Date::Now().toString() + " " + Clock::Now().toString() + "\r\n";
+		logMessage += "SPI - " + Date::Now().toString() + " " + Clock::Now().toString() + "\r\n";
 		
 		m_file.writeLine(logMessage);
 	}
