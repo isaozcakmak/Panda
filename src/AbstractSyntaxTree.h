f3b3352ba9b41e3f88150c9e0ad562ff349eb02e
Isa Ozcakmak <isaozcakmak79@gmail.com>
2021-01-23 11:05:45 +0300

    The codes that were in comment were deleted

--- src/AbstractSyntaxTree.h
+++ src/AbstractSyntaxTree.h
@@ -24,30 +24,9 @@ public:
 	};
 
 	AbstractSyntaxTree();
-//	AbstractSyntaxTree(Token token);
-//	AbstractSyntaxTree(AbstractSyntaxTree* left, Token op, AbstractSyntaxTree* right);
-//	AbstractSyntaxTree(Token op, AbstractSyntaxTree* expr);
 	~AbstractSyntaxTree() {}
-//
-//	AbstractSyntaxTree& operator=(const AbstractSyntaxTree &other);
-//
+
 	virtual NodeType getNodeType() = 0;
-//	int getTokenValue();
-//	Token::TokenType getOpTokenType();
-//	AbstractSyntaxTree* getLeft();
-//	AbstractSyntaxTree* getRight();
-//	AbstractSyntaxTree* getExpr();
-//
-//private:
-//	NodeType m_nodeType;
-//
-//	Token m_token;
-//
-//	AbstractSyntaxTree* m_left;
-//	AbstractSyntaxTree* m_right;
-//	Token m_op;
-//
-//	AbstractSyntaxTree* m_expr;
 
 };
 
